0b010364721babfde6dac8ac2e5dcebafbc9ba5a
Kelevra Mad <32633505+kelevramad@users.noreply.github.com>
2017-11-01 20:30:08 -0300

    Create win.sh

---
+++ win.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cat /dev/null > ~/.bash_history && history -c
+cd /root/Desktop/fsociety/
+gnome-terminal --geometry=105x26+0+0
+gnome-terminal --geometry=105x26+960+0
+gnome-terminal --geometry=105x26+0+600
+gnome-terminal --geometry=105x26+960+600
