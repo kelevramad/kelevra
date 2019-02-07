22e581b000227d259e5a551f9ca327f243c0f0ae
Kelevra Mad <32633505+kelevramad@users.noreply.github.com>
2019-02-07 17:03:00 -0200

    Add files via upload

--- flip.py
+++ flip.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import argparse
 from PIL import Image
 
 __author__ = "Center For Cyber Intelligence - Central Intelligence Agency"
@@ -18,7 +19,19 @@ CGREY = '\033[90m'
 CBLAC = '\033[90m'
 CEND = '\033[0m'
 
-VERSION = '%s[+]%s %s - Version: %s' %(CGREEN, CEND, __description__, __version__) 
+__version = '%s[+]%s %s - Version: %s' %(CGREEN, CEND, __description__, __version__) 
+
+def get_args():
+	parser = argparse.ArgumentParser(description=__description__)
+	parser.add_argument('-f', '--file', dest='file', help='file(s) for flip image(s).', type=argparse.FileType('r'), nargs='*')
+	parser.add_argument('-d', '--debug', dest='debug', help='This argument allows debugging information.', action='store_true')
+	parser.add_argument('-v', '--version', dest='version', help='This argument show version.', action='version', version=__version)
+
+	if len(sys.argv) == 1:  # If no arguments were provided, then print help and exit.
+		parser.print_help()
+		sys.exit(1)
+
+	return parser.parse_args()
 
 def flip_image(image_path, saved_location):
 	"""
@@ -32,7 +45,22 @@ def flip_image(image_path, saved_location):
 	print "%s[+]%s File => Flipped: %s => %s" %(CGREEN, CEND, image_path, saved_location)
 	#rotated_image.show()
 
+def main():
+	os.system('clear')
+
+	global args
+
+	args = get_args()
+
+	if args.debug:
+		print "%s[!]%s Mode Debug On" %(CYELLOW, CEND)
+
+
+	if args.file:
+		for f in args.file:
+			str_path, str_filename = os.path.split(f.name)
+			flip_image(str_path + '/' + str_filename, str_path + '/F_' + str_filename)
+
 if __name__ == '__main__':
-	str_path, str_filename = os.path.split(sys.argv[1])
-	flip_image(str_path + '/' + str_filename, str_path + '/F_' + str_filename)
+	main()
 
